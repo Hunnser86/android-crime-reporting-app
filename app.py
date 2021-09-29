3705db2700c7acab60a35795555c97ef3064b274
Hunnser86 <robhunns@gmail.com>
2021-09-29 18:09:41 +0000

    Remove authentication functonality and re-start project

--- app.py
+++ app.py
@@ -12,105 +12,30 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-
 mongo = PyMongo(app)
 
 
-@app.route("/home")
-def go_home():
-    return render_template("home.html")
-
-
 @app.route("/")
-@app.route("/get_reports")
+@app.route("/reports")
 def get_reports():
-    reports = mongo.db.reports.find()
-    return render_template("reports.html", reports=reports)
-
-
-"""
-Functionality for registering.  Taken from the task manager walkthrough project
-
-"""
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # check if username already exists
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            flash(
-            "Username already exists. Do not attempt to user another officer's credentials!")
-            return redirect(url_for("register"))
-
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-            }
-    
-        mongo.db.users.insert_one(register)
-
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Officer Registration Complete")       
-    return render_template("register.html")
-
-
-"""
-Functonality for login. Taken from the task manager walkthrough project
-"""
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        # check if username exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            # ensure hashed password matches user input
-            if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(
-                            request.form.get("username")))
-                return redirect(url_for(
-                            "profile", username=session["user"]))
-            else:
-                # invalid password match
-                flash("Incorrect Username and/or Password")
-                return redirect(url_for("login"))
-
-        else:
-            # username doesn't exist
-            flash("Incorrect Username and/or Password")
-            return redirect(url_for("login"))
-
-    return render_template("login.html")
-
-
-"""
-Functonality for logout. Taken from the task manager walkthrough project
-"""
-
-
-@app.route("/logout")
-def logout():
-    # remove user from session cookie
-    flash("You have been logged out")
-    session.pop("user")
-    return redirect(url_for("login"))
+    reports = mongo.db.tasks.find()
+    return  render_template("reports.html", reports=reports)   
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)   
+            debug=True)
