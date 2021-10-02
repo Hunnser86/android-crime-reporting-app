992714af91dd1c166b468660fc3e34b44804a343
Hunnser86 <robhunns@gmail.com>
2021-10-02 12:46:17 +0000

    Finish importing modules from flask, connect app to mongodb and create @app.route for home.html

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask flash, render_template,
+     redirect, request, session, url_for)
+from flask_pymontgo import PyMongo
+from bson.objectid import ObjectId 
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +11,18 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "THE ANDROIDS ARE TAKING OVER!"
+@app.route("/go_home")
+def go_home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
